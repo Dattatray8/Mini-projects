0298af268cedf1616392201b177eeb51c4d0579f
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-07-04 10:32:21 +0530

    added tailwind

--- 12. basic react project/src/App.jsx
+++ 12. basic react project/src/App.jsx
@@ -7,24 +7,25 @@ function App() {
   const [val, setVal] = useState('')
   const [currentData, filterData] = useState(data)
   return (
-    <>
-      <h1>Aalu lelo aalu lelo</h1>
-      <div>
+    <div className='flex flex-col justify-center gap-7 items-center min-h-screen'>
+      <h1 className='text-4xl font-bold p-5'>Aalu lelo aalu lelo</h1>
+      <div className='flex w-full justify-center items-center'>
         <input type="text" placeholder='kya mangta ?' onChangeCapture={(e) => {
           setVal(e.target.value);
           filterData(data.filter(card => {
             return card.title.toLowerCase().includes(val.toLowerCase())
           }))
-        }} />
+        }} className='border py-1 px-3 rounded-l-3xl focus:outline-none' />
         <button onClick={() => {
           filterData(data.filter(card => {
             if (card.title.toLowerCase().includes(val.toLowerCase())) {
               return card
             }
           }))
-        }}>lelo</button></div>
+        }} className='bg-[#181717] text-white py-[5px] rounded-r-3xl px-3 hover:bg-[#181717ce]'>lelo</button>
+      </div>
       <AllCards cards={currentData} />
-    </>
+    </div>
   )
 }
 
--- 12. basic react project/src/components/AllCards.jsx
+++ 12. basic react project/src/components/AllCards.jsx
@@ -1,15 +1,17 @@
 export default function AllCards({ cards }) {
     return (
         <>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', margin: '20px' }}>
+            <div className="flex flex-wrap gap-6 justify-center items-center p-5 w-[90%]">
                 {
                     (cards.length === 0) ? 'No product available' :
                         cards.map(card => {
-                            return <div style={{ backgroundColor: 'gray', width: '300px', padding: '10px' }} key={card.id}>
-                                <img src={card.images} alt="" style={{ width: '100px' }} />
-                                <p>Name: {card.title}</p>
-                                <p>Price: ${card.price}</p>
-                                <p>Description: {card.description}</p>
+                            return <div className="w-[17em] sm:w-[22em] h-[25em] bg-[#ececec] p-3 flex flex-col justify-center gap-2 rounded-2xl" key={card.id}>
+                                <img src={card.images} alt="" className="w-[10em] h-fit self-center" />
+                                <div className="flex justify-between">
+                                    <p className="opacity-70">{card.title}</p>
+                                    <p className="opacity-70">${card.price}</p>
+                                </div>
+                                <p className="opacity-70">{card.description}</p>
                             </div>
                         })
                 }
